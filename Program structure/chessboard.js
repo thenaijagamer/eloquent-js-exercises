d5e85063d34fa859144557b4787c0dec66a8d2d6
thenaijagamer <sabdullahialaba50@gmail.com>
2022-08-28 08:59:22 +0100

    Refactor to code structure to shorter code structure.

--- Program structure/chessboard.js
+++ Program structure/chessboard.js
@@ -2,25 +2,11 @@ let chessBoard = "";
 const size = 8;
 
 for (let lineCount = 1; lineCount <= size; lineCount++) {
-  if (lineCount % 2 === 0) {
-    // Run if line number is even
-    for (let charCount = 1; charCount <= size; charCount++) {
-      if (charCount % 2 === 0) {
-        // Run if char number is even
-        chessBoard += " ";
-      } else {
-        // Run if char number is odd
-        chessBoard += "#";
-      }
-    }
-  } else {
-    // Run if line number is odd
-    for (let charCount = 1; charCount <= size; charCount++) {
-      if (charCount % 2 === 0) {
-        chessBoard += "#";
-      } else {
-        chessBoard += " ";
-      }
+  for (let charCount = 1; charCount <= size; charCount++) {
+    if ((charCount + lineCount) % 2 === 0) {
+      chessBoard += " ";
+    } else {
+      chessBoard += "#";
     }
   }
   chessBoard += "\n";
