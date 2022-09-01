5902459d1fa2721576bac89b2091333fa9be51cd
thenaijagamer <sabdullahialaba50@gmail.com>
2022-09-01 11:51:19 +0100

    Add data structure folder and create sum of range program and add modification to recursion program.

---
+++ functions/recursion.js
@@ -0,0 +1,14 @@
+// Fixed isEven recursive function
+function isEven(num) {
+  if (num === 0) {
+    return true;
+  } else if (num === 1) {
+    return false;
+  } else if (num < 0) {
+    return isEven(num + 2);
+  } else {
+    return isEven(num - 2);
+  }
+}
+
+console.log(isEven(-2));
