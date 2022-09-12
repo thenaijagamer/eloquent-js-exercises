0b1c6e8a8b3658250e24b47769f4249c2806a494
thenaijagamer <sabdullahialaba50@gmail.com>
2022-09-12 09:12:45 +0100

    Fixing another bug in reverse array program

--- Data Structures Objects and Arrays/reversing-an-array.js
+++ Data Structures Objects and Arrays/reversing-an-array.js
@@ -6,15 +6,17 @@ function reverseArray(array) {
   return newArray;
 }
 
-let newArray = [1, 2, 3, 4, 5];
+let newArray = [1, 2, 3, 4, 5, "b", "c", 4];
 function reverseArrayInPlace() {
+  let newArrayI = [];
   for (let i = 0, counter = 1; i < newArray["length"]; i++, counter++) {
-    newArray[i].push(array[array["length"] - counter]);
+    newArrayI.push(newArray[newArray["length"] - counter]);
   }
+  newArray = newArrayI;
 }
 console.log(reverseArray(["a", "b", "c"]));
 // -> [ 5, 4, 3, 2, 1 ]
 
-// reverseArrayInPlace();
-// console.log(newArray);
+reverseArrayInPlace();
+console.log(newArray);
 // -> [ 5, 4, 3, 2, 1 ]
