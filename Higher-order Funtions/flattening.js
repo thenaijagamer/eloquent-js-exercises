881d777ac5d84ad60bf1565c5898a2a898cf6674
thenaijagamer <sabdullahialaba50@gmail.com>
2022-09-15 01:07:39 +0100

    Refactoring flattening program

--- Higher-order Funtions/flattening.js
+++ Higher-order Funtions/flattening.js
@@ -1,7 +1,8 @@
+// higher-order function to concatenate arrays in an array
 let arrays = [[1, 2, 3], [4, 5], [6]];
-const flat = arrays.reduce((a, b) => {
-  return a.concat(b);
-});
+const flat = arrays.reduce((flat, current) => {
+  return flat.concat(current);
+}, []);
 console.log(flat);
 
 // → [1, 2, 3, 4, 5, 6]
