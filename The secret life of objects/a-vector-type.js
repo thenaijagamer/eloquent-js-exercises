187ce5fc43da05868b30526303b24df4811dc418
thenaijagamer <sabdullahialaba50@gmail.com>
2022-09-16 22:32:13 +0100

    Creating a new program a-vector-type inside a new chapter The secret life of objects

---
+++ The secret life of objects/a-vector-type.js
@@ -0,0 +1,23 @@
+// Your code here.
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(NewVec) {
+    return { x: this.x + NewVec.x, y: this.y + NewVec.y };
+  }
+  minus(NewVec) {
+    return { x: this.x - NewVec.x, y: this.y - NewVec.y };
+  }
+  get length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
