f54756e29d7aa8dccf3b4394ce6d3c0e0073f4fd
thenaijagamer <sabdullahialaba50@gmail.com>
2022-09-18 15:34:07 +0100

    Abandoning iterable-group.js

--- The secret life of objects/iterable-groups.js
+++ The secret life of objects/iterable-groups.js
@@ -1,31 +1,31 @@
-// Your code here (and the code from the previous exercise)
-class Group {
-  constructor() {
-    this.arr = [];
-  }
-  add(val) {
-    if (!this.has(val)) this.arr.push(val);
-  }
-  delete(val) {
-    this.arr = this.arr.filter((el) => el !== val);
-  }
-  has(val) {
-    return this.arr.includes(val);
-  }
-  static from(arr) {
-    let group = new Group(arr);
-    for (const e of arr) {
-      group.add(e);
-    }
-    return group;
-  }
-}
+// // Your code here (and the code from the previous exercise)
+// class Group {
+//   constructor() {
+//     this.arr = [];
+//   }
+//   add(val) {
+//     if (!this.has(val)) this.arr.push(val);
+//   }
+//   delete(val) {
+//     this.arr = this.arr.filter((el) => el !== val);
+//   }
+//   has(val) {
+//     return this.arr.includes(val);
+//   }
+//   static from(arr) {
+//     let group = new Group(arr);
+//     for (const e of arr) {
+//       group.add(e);
+//     }
+//     return group;
+//   }
+// }
 
-class IterableGroups {}
+// class IterableGroups {}
 
-for (let value of Group.from(["a", "b", "c"])) {
-  console.log(value);
-}
-// → a
-// → b
-// → c
+// for (let value of Group.from(["a", "b", "c"])) {
+//   console.log(value);
+// }
+// // → a
+// // → b
+// // → c
